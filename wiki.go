b5455dd8eb64687b350371a80a692f035ab34626
chris <chris.ng93@gmail.com>
2017-07-25 14:23:30 -0700

    validation

--- wiki.go
+++ wiki.go
@@ -4,8 +4,11 @@ import (
   "io/ioutil"
   "net/http"
   "html/template"
+  "regexp"
+  "errors"
 )
 
+// Page data structure
 type Page struct {
   Title string
   Body []byte
@@ -25,13 +28,34 @@ func loadPage(title string) (*Page, error) {
   return &Page{Title: title, Body: body}, nil
 }
 
-func renderTemplate(w http.ResponseWriter, template string, p *Page) {
-  t, _ := template.ParseFiles(template + ".html")
-  t.Execute(w, p)
+// Templating
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+  err := template.ExecuteTemplate(w, tmpl+".html", p)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
 }
 
+// Validation
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+  m := validPath.FindStringSubmatch(r.URL.Path)
+  if m == nil {
+    http.NotFound(w, r)
+    return "", errors.New("Invalid Page Title")
+  }
+  return m[2], nil
+}
+
+// Route handlers
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-  title := r.URL.Path[len("/view/"):]
+  title, err := getTitle(w, r)
+  if err != nil {
+    return
+  }
   p, err := loadPage(title)
   if err != nil {
     http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -41,7 +65,10 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-  title := r.URL.Path[len("/edit/"):]
+  title, err := getTitle(w, r)
+  if err != nil {
+    return
+  }
   p, err := loadPage(title)
   if err != nil {
     p = &Page{Title: title}
@@ -50,10 +77,17 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-  title := r.URL.Path[len("/save/"):]
+  title, err := getTitle(w, r)
+  if err != nil {
+    return
+  }
   body := r.FormValue("body")
   p := &Page{Title: title, Body: []byte(body)}
-  p.save()
+  err := p.save()
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
   http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
